ee81da95730ea0e12ed067b89e385a5befce0160
Nour Abdellatif <nab@luftborn.com>
2023-08-15 12:58:14 +0300

    Initial commit

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ProductListComponent} from "./products/product-list.component";
+import {WelcomeComponent} from "./home/welcome.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
